5d8809fc759de49547a1342f0c4228aa626d7f0a
Desislava Naydenova <deskata.01@gmail.com>
2024-08-20 09:56:19 +0000

    Create a registration form

--- hello_alps/urls.py
+++ hello_alps/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('menu_list', views.MenuList.as_view(), name='menu_list'),
     path('opening_hours', views.OpeningHour, name='opening_hours'),
     path('reservations/', views.reservations, name='reservations'),
-    path('login/', Login.as_view(), name='login'),
+    path('login/', Login.as_view(), name='login_url'),
+    path('register/' , register, name='signup_url')
     ]
